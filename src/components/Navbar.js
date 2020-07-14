ba69896e13feb98500fe40e97aec584573ce2304
Jonny Chang <jonathanchang0805@live.ca>
2020-07-13 18:08:41 -0700

    Added logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types'
 
 // Mui
 import AppBar from '@material-ui/core/AppBar';
@@ -7,17 +9,54 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+// Redux
+import { connect } from 'react-redux';
+import { logoutUser, getUserData } from '../redux/actions/userActions';
+
+const styles = {
+    button: {
+        float: 'right',
+        position: ''
+    },
+    header: {
+        margin: 'auto'
+    }
+}
 
 class Navbar extends Component {
+    handleLogout = () => {
+        this.props.logoutUser()
+        window.location.href = '/login';
+        console.log('logged out')
+    }
     render() {
+        const { classes } = this.props
         return (
             <AppBar position="fixed">
-                <Toolbar className='nav-container'>
-                    <h2>Community Posterboard</h2>
+                <Toolbar>
+                    <div className={classes.header}>
+                        <h2>Community Posterboard</h2>
+                    </div>
+                    <Button className={classes.button} color='inherit' onClick={this.handleLogout}>
+                        Logout
+                    </Button>
                 </Toolbar>
             </AppBar>   
         )
     }
 }
 
-export default Navbar
+Navbar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired
+}
+
+const mapStatesToProps = (state) => ({
+    user: state.user
+})
+
+const mapActionsToProps = {
+    logoutUser
+}
+
+export default connect(mapStatesToProps, mapActionsToProps)(withStyles(styles)(Navbar))
